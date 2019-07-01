7fcacbfb11c42591fd87ca9567463608874594ff
HaiboZhu <hbzhu@thoughtworks.com>
2019-07-01 22:00:33 +0800

    finish practice 03.

--- src/main/java/practice03/Worker.java
+++ src/main/java/practice03/Worker.java
@@ -1,4 +1,23 @@
 package practice03;
 
 public class Worker {
+    private int age;
+    private String name;
+
+    public Worker(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public String introduce() {
+        return "I am a Worker. I have a job.";
+    }
 }
