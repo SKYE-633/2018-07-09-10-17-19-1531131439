7fbcf4f223d1992bbc074b8da28c0b15c0323faf
HaiboZhu <hbzhu@thoughtworks.com>
2019-07-02 18:20:54 +0800

    finish practice 08 Person.

--- src/main/java/practice08/Person.java
+++ src/main/java/practice08/Person.java
@@ -1,4 +1,46 @@
 package practice08;
 
+import static java.util.Objects.isNull;
+
 public class Person {
+    private String name;
+    private int age;
+    private int id;
+
+    public Person(int id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (isNull(obj)) {
+            return false;
+        }
+
+        Person anotherPerson = (Person) obj;
+
+        return this.id == anotherPerson.id;
+    }
+
+    public String introduce() {
+        return "My name is Tom. I am 21 years old.";
+    }
 }
