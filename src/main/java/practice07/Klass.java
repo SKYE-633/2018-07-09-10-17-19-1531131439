de3696bbcc02bb0d17ef296284f3bae1d0af879f
HaiboZhu <hbzhu@thoughtworks.com>
2019-07-02 18:13:28 +0800

    finish practice 07.

--- src/main/java/practice07/Klass.java
+++ src/main/java/practice07/Klass.java
@@ -1,4 +1,26 @@
 package practice07;
 
 public class Klass {
+    private int number;
+    private String displayName;
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getDisplayName() {
+        return String.format("Class %s", this.number);
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
 }
--- src/main/java/practice07/Teacher.java
+++ src/main/java/practice07/Teacher.java
@@ -1,4 +1,39 @@
 package practice07;
 
-public class Teacher {
+import static java.util.Objects.isNull;
+
+public class Teacher extends Person {
+    private Klass klass;
+
+    public Teacher(String name, int age, Klass klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public void setKlass(Klass klass) {
+        this.klass = klass;
+    }
+
+    @Override
+    public String introduce() {
+        final String introduce = super.introduce();
+        return String.format("%s I am a Teacher. I teach %s.", introduce, isNull(klass)
+                ? "No Class" : this.klass.getDisplayName());
+    }
+
+    public String introduceWith(Student jerry) {
+        final String introduce = super.introduce();
+
+        return String.format("%s I am a Teacher. I %s %s.", introduce,
+                this.klass.getNumber() == jerry.getKlass().getNumber()
+                ? "teach" : "don't teach", jerry.getName());
+    }
 }
