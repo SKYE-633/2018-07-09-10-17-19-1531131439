f8bb3842067ccdcd534b28642956f13b56ab5887
HaiboZhu <hbzhu@thoughtworks.com>
2019-07-03 09:34:53 +0800

    finish practice 11 some test.

--- src/main/java/practice11/Klass.java
+++ src/main/java/practice11/Klass.java
@@ -1,4 +1,43 @@
 package practice11;
 
 public class Klass {
+    private int number;
+    private String displayName;
+    private Student leader;
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getDisplayName() {
+        return "Class " + this.number;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public void assignLeader(Student student) {
+        if (student.getKlass().getNumber() != this.number) {
+            System.out.println("It is not one of us.");
+        } else {
+            this.leader = student;
+        }
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public void setLeader(Student student) {
+        this.leader = student;
+    }
 }
--- src/main/java/practice11/Person.java
+++ src/main/java/practice11/Person.java
@@ -1,4 +1,53 @@
 package practice11;
 
+import static java.util.Objects.isNull;
+
 public class Person {
+    private String name;
+    private int age;
+    private int id;
+
+    public Person(int id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (isNull(obj)) {
+            return false;
+        }
+
+        Person person = (Person) obj;
+        return this.id == person.getId();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String introduce() {
+        return "My name is Tom. I am 21 years old.";
+    }
 }
