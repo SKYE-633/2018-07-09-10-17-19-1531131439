9ab4f72aafaa4d2f219694c9f1fd2fecbe84f05b
Lushanshan336 <51399970+Lushanshan336@users.noreply.github.com>
2019-07-11 23:44:12 +0800

    Update Student.java

--- src/main/java/practice06/Student.java
+++ src/main/java/practice06/Student.java
@@ -1,4 +1,20 @@
 package practice06;
 
-public class Student {
+public class Student extends Person{
+    public Student() {
+
+        int klass;
+    }
+    public Student(){
+
+    }
+    public Student(String name, int age){
+        super(name,age);
+    }
+
+    public String introduce(){
+        return "My name is Tom. I am 21 years old."+"I am a Student. I am at Class 2";
+    }
+
+
 }
