23867f56c95cef66e5d7cc85948b9435c33349d4
Lushanshan336 <51399970+Lushanshan336@users.noreply.github.com>
2019-07-11 23:29:24 +0800

    Update Person.java

--- src/main/java/practice04/Person.java
+++ src/main/java/practice04/Person.java
@@ -1,4 +1,22 @@
 package practice04;
 
 public class Person {
+  private String name;
+    private int age;
+    public Person(){
+        name = "Tom";
+        age = 21;
+    }
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+    public String introduce(){
+        return "My name is"+name+"."+"I am"+age+"years old.";
+    }
+
+    public void basicIntroduce(){
+        return;
+    }
 }
