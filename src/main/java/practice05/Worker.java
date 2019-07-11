2a0b6436383c585249a57d1e118b15c6d0be46ef
Lushanshan336 <51399970+Lushanshan336@users.noreply.github.com>
2019-07-11 23:38:11 +0800

    Update Worker.java

--- src/main/java/practice05/Worker.java
+++ src/main/java/practice05/Worker.java
@@ -1,4 +1,15 @@
 package practice05;
 
-public class Worker {
+public class Worker extends Person{
+
+    public Worker(){
+
+    }
+    public Worker(String name, int age){
+        super(name,age);
+    }
+    public String introduce(){
+        return "My name is Tom. I am 21 years old.";
+    }
+
 }
