9401fbcc2b0831e2618d624d7503da7492544989
Lushanshan336 <51399970+Lushanshan336@users.noreply.github.com>
2019-07-11 23:44:35 +0800

    Update Teacher.java

--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,19 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person{
+
+    public Teacher(){
+        int klass;
+    }
+    public Teacher(String name, int age){
+        super(name,age);
+    }
+    public String introduce(){
+        if (int klass= ){
+            return "My name is Tom. I am 21 years old. I am a Teacher. I teach No Class."
+        }else{
+            return "My name is Tom. I am 21 years old.I am a Teacher. I teach Class 2.";
+        }
+    }
+
 }
