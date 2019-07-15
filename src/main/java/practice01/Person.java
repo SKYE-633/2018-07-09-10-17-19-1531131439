01651c133747d31cdeb4dd8aed3d9e0e467393b0
homework <52529600+SKYE-633@users.noreply.github.com>
2019-07-15 08:38:30 +0800

    Update Person.java

--- src/main/java/practice01/Person.java
+++ src/main/java/practice01/Person.java
@@ -3,16 +3,28 @@ package practice01;
 public class Person {
   private String name;
     private int age;
-    public Person(){
-        name = "Tom";
-        age = 21;
-    }
+   
 
     public Person(String name, int age) {
         this.name = name;
         this.age = age;
     }
+  public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
     public String introduce(){
-        return "My name is"+name+"."+"I am"+age+"years old.";
+        return "My name is Tom. I am 21 years old.";
     }
 }
